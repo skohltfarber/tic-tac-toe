d84d2e66e54205447ca214851f47a8cfd3d0b67e
Shawn Kohltfarber <shawn.kohltfarber@gmail.com>
2021-02-21 22:07:32 -0600

    Updates

---
+++ precache-manifest.3ce9329e48e67598fdffc481c051ccc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a4ec22d3d5208e4160c",
+    "url": "/tic-tac-toe/static/css/main.99bfa7bc.chunk.css"
+  },
+  {
+    "revision": "4a4ec22d3d5208e4160c",
+    "url": "/tic-tac-toe/static/js/main.4a4ec22d.chunk.js"
+  },
+  {
+    "revision": "3a7d76090da357ba178f",
+    "url": "/tic-tac-toe/static/js/1.3a7d7609.chunk.js"
+  },
+  {
+    "revision": "4292f68d6aadb5c6feaf",
+    "url": "/tic-tac-toe/static/js/2.4292f68d.chunk.js"
+  },
+  {
+    "revision": "24d2051b91ccc700ba7d",
+    "url": "/tic-tac-toe/static/js/runtime~main.24d2051b.js"
+  },
+  {
+    "revision": "73c6c529c443b385b8230674e13794a9",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
